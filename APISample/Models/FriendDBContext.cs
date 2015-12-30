ba54e375af1a715ca3446a83c5003f9f9d7a2dee
aslamansari <aslam.life@gmail.com>
2015-12-30 15:16:26 +0530

    Revert "Removed files"
    This reverts commit ba8d98973113ccdd24646f0e45552ea08b30341f.

---
+++ APISample/Models/FriendDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APISample.Models
+{
+    public class FriendDBContext : DbContext
+    {
+        public FriendDBContext()
+            : base("myConnection")
+        {
+            base.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<Friend> Friends { get; set; }
+    }
+}
